5584a96866657a005a8ccb460f5969e098d93e7d
Carl Ansley <carl.ansley@gmail.com>
2024-01-08 14:47:33 -0500

    whitespace

--- src/operator/index.ts
+++ src/operator/index.ts
@@ -7,6 +7,7 @@
  */
 
 import type { Asyncerator } from '../asyncerator';
+
 export type Operator<Input, Output> = (input: Asyncerator<Input>) => Asyncerator<Output>;
 
 export { default as forEach } from './for-each';
